e649a01ddcabf59b0bf211329acc2c0d4f787d3c
Moksha <mokshyagnay@gmail.com>
2025-08-02 19:45:09 +0530

    smart: Update frontend and package.json files

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import Home from './Home'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FirstPage from './FirstPage';
 export default function App() {
   return (
