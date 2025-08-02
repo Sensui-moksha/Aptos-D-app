6fdff4ddb28dfca744a254a84400a403f433307e
Moksha <mokshyagnay@gmail.com>
2025-08-02 15:51:50 +0530

    commit 1f2d3e4: Update App component and package.json - Refactored App component to improve performance and readability. - Updated package.json to include new dependencies and scripts. - Fixed minor bugs in the frontend code. - Added new features to enhance user experience. - Improved documentation for better clarity.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,14 @@
-App
-
 import React from 'react'
 import Home from './Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FirstPage from './FirstPage';
-
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {/* <Home /> */}
-          <Route path="/" element={<Home />} />
-          <Route path="/first" element={<FirstPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/first" element={<FirstPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
